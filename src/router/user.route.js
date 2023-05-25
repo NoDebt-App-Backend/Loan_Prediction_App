06bd1e796e87dc8f4563602ed39efd6e03f8c168
Perpy-del <perpetualmeninwa@gmail.com>
2023-05-25 22:52:45 +0100

    Added a route to find a user

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -1,8 +1,8 @@
 import express from "express";
 const router = express.Router();
-import { tryCatchHandler } from '../utils/tryCatchHandler.js';
+import { tryCatchHandler } from "../utils/tryCatchHandler.js";
 import UserController from "../controllers/user.controller.js";
-import authMiddleware  from '../middlewares/auth.js';
+import authMiddleware from "../middlewares/auth.js";
 
 // route to create a new user
 router.post("/create", tryCatchHandler(UserController.createUser));
@@ -10,9 +10,13 @@ router.post("/create", tryCatchHandler(UserController.createUser));
 router.get("/", tryCatchHandler(UserController.findUser));
 
 //route to login and get jwt token
-router.post('/login', tryCatchHandler(UserController.Login));
+router.post("/login", tryCatchHandler(UserController.Login));
 
 //protected route that grant access after successful login
-router.get('/protected', authMiddleware, tryCatchHandler(UserController.protectedRoute));
+router.get(
+  "/protected",
+  authMiddleware,
+  tryCatchHandler(UserController.protectedRoute)
+);
 
 export default router;
