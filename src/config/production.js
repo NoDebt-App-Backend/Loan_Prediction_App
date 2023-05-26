ea5cdb7b7b44d8493971b450e5eb0fdf3311021f
Perpy-del <perpetualmeninwa@gmail.com>
2023-05-26 13:21:24 +0100

    Add forgot password

--- src/config/production.js
+++ src/config/production.js
@@ -1,12 +1,12 @@
 export const production = {
-  database_url: process.env.STAGING_MONGODB_CONNECTION_URL,
-  bcrypt_saltRound: +process.env.STAGING_BCRYPT_SALT_ROUND,
-  jwt_access: process.env.STAGING_JWT_KEY,
+  database_url: process.env.PROD_MONGODB_CONNECTION_URL,
+  bcrypt_saltRound: +process.env.PROD_BCRYPT_SALT_ROUND,
+  jwt_access: process.env.PROD_JWT_KEY,
   port: process.env.PORT,
-  nodemailer_host: process.env.STAGING_NODEMAILER_HOST,
-  nodemailer_user: process.env.STAGING_NODEMAILER_USER,
-  nodemailer_pass: process.env.STAGING_NODEMAILER_PASS,
-  nodemailer_service: process.env.STAGING_NODEMAILER_SERVICE,
-  base_url: process.env.STAGING_NODEMAILER_BASE_URL,
-  password_secretkey: process.env.STAGING_NODEMAILER_UPDATE_PASSWORD_SECRET_KEY
+  nodemailer_host: process.env.PROD_NODEMAILER_HOST,
+  nodemailer_user: process.env.PROD_NODEMAILER_USER,
+  nodemailer_pass: process.env.PROD_NODEMAILER_PASS,
+  nodemailer_service: process.env.PROD_NODEMAILER_SERVICE,
+  base_url: process.env.PROD_NODEMAILER_BASE_URL,
+  password_secretkey: process.env.PROD_NODEMAILER_UPDATE_PASSWORD_SECRET_KEY
 };
