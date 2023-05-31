7886a3a3d1df018919163473f0f3d5c3927b5087
Perpy-del <perpetualmeninwa@gmail.com>
2023-05-31 11:36:08 +0100

    Added CORS

--- app.js
+++ app.js
@@ -35,18 +35,18 @@ app.get("/api", (req, res) => {
   res.send("Welcome to NoDebt App");
 });
 
-// app.use(cors());
+app.use(cors());
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  // if (allowedOrigins.includes(origin)) {
-  //   res.setHeader("Access-Control-Allow-Origin", origin);
-  // }
-  // res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
-  // res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  // res.header("Access-Control-Allow-Credentials", true);
-  // return next();
-});
+// app.use((req, res, next) => {
+//   const origin = req.headers.origin;
+//   // if (allowedOrigins.includes(origin)) {
+//   //   res.setHeader("Access-Control-Allow-Origin", origin);
+//   // }
+//   // res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+//   // res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   // res.header("Access-Control-Allow-Credentials", true);
+//   // return next();
+// });
 
 // defining the routes
 app.use("/api/users", router);
