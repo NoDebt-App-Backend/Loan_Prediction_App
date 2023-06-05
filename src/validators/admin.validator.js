f147ff66cc5b29efdba680c1eb79c3cb157deab4
Perpy-del <perpetualmeninwa@gmail.com>
2023-06-05 20:44:24 +0100

    Double checked the names that it matches what shows up on the schema to fix the bug

--- src/validators/admin.validator.js
+++ src/validators/admin.validator.js
@@ -1,50 +1,3 @@
-// import Joi from "joi";
-
-// // using Joi to create validation for the user being created before sent to database
-// const createCompanyValidator = Joi.object({
-//   companyName: Joi.string().required(),
-//   firstName: Joi.string().required(),
-//   email: Joi.string()
-//     .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
-//     .required()
-//     .messages({
-//       "string.pattern.base":
-//         "Not a valid email address. Please input a valid email address.",
-//     }),
-//   password: Joi.string()
-//     .regex(
-//       /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]).{8,}$/
-//     )
-//     .required()
-//     .messages({
-//       "string.pattern.base":
-//         "Password must be more than 8 characters long with at least one number, one alphanumeric character, one uppercase letter",
-//     }),
-//   confirmPassword: Joi.any()
-//     .equal(Joi.ref("password"))
-//     .required()
-//     .label("Passwords")
-//     .messages({ "any.only": "{{#label}} do not match. Please check again" })
-// }).strict();
-
-// const loginAdminValidator = Joi.object(
-//   {
-//     email: Joi.string()
-//     .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
-//     .required()
-//     .messages({
-//       "string.pattern.base":
-//         "Not a valid email address. Please input a valid email address.",
-//     }),
-//     password: Joi.string().required().messages({
-//       "string.pattern.base":
-//         "Not a valid email address or password",
-//     })
-//   }
-// )
-
-// export  {createCompanyValidator, loginAdminValidator} ;
-
 import Joi from "joi";
 
 // using Joi to create validation for the user being created before sent to database
@@ -69,7 +22,7 @@ const createCompanyValidator = Joi.object({
     }),
   confirmPassword: Joi.any()
     .equal(Joi.ref("password"))
-    .required()
+    // .required()
     .label("Passwords")
     .messages({ "any.only": "{{#label}} do not match. Please check again" }),
   organisationName: Joi.string().required(),
