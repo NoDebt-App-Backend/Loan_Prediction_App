22f696f246e94387a814420f8fde6de959801f68
Perpy-del <perpetualmeninwa@gmail.com>
2023-06-07 05:25:38 +0100

    Pushing changes

--- src/validators/admin.validator.js
+++ src/validators/admin.validator.js
@@ -26,7 +26,7 @@ const createCompanyValidator = Joi.object({
     .label("Passwords")
     .messages({ "any.only": "{{#label}} do not match. Please check again" }),
   organisationName: Joi.string().required(),
-  password: Joi.string()
+  passwordLink: Joi.string()
 }).strict();
 
 const loginAdminValidator = Joi.object({
