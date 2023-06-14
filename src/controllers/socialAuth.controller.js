190bd86f0f4907aee454cd99de07e9a0e9777576
Perpy-del <perpetualmeninwa@gmail.com>
2023-06-14 08:19:07 +0100

    New changes

--- src/controllers/socialAuth.controller.js
+++ src/controllers/socialAuth.controller.js
@@ -12,44 +12,70 @@ passport.use(
       callbackURL: config.google_callback,
       passReqToCallback: true,
     },
-    async function (accessToken, refreshToken, profile, cb) {
-      const adminGoogle = Admin.findOne({
+    async function (accessToken, refreshToken, profile, done) {
+      const adminGoogle = await AdminGoogle.findOne({
         googleId: profile.id,
       });
 
-      if (!adminGoogle) {
-        const adminGoogle = new AdminGoogle({
-          googleId: profile.id,
-          // firstName: profile.givenName,
-          // lastName: profile.familyName,
-          profileImage: profile.picture,
-          email: profile.emails[0].value,
-        //   accessToken: accessToken,
-          // provider: profile.provider,
-          // passwordLink: req.body.passwordLink,
-        });
-        await adminGoogle.save();
-        // console.log(profile);
+      if (adminGoogle) {
+        done(null, adminGoogle)
+      } else {
+        if (req.body.email && req.body.password) {
+          const email = profile.email[0].value;
+          const password = req.body.password;
+
+          const admin = new AdminGoogle({
+            email,
+            password,
+            googleId: profile.id,
+            name: profile.displayName
+          });
+
+          adminGoogle = await admin.save();
+
+          done(null, adminGoogle);
+        } else {
+          done(null, false, {
+            message: 'Please signup with Google or your email address'
+          })
+        }
       }
 
-      const { googleId, _id, createdAt, updatedAt, email, passwordLink } = adminGoogle;
-  //           // Return a response to the client
-            res.status(200).json({
-              message: "Account created successfully",
-              status: "Success",
-              data: {
-               adminGoogle: {
-  //                 firstName: firstName,
-  //                 lastName: lastName,
-                  email: email,
-                  AdminId: _id,
-                  GoogleId: googleId,
-                  createdAt: createdAt,
-                  updatedAt: updatedAt,
-                  passwordLink: passwordLink,
-                },
-              },
-            });
+      if (error) throw error;
+
+  //     if (!adminGoogle) {
+  //       const adminGoogle = new AdminGoogle({
+  //         googleId: profile.id,
+  //         // firstName: profile.givenName,
+  //         // lastName: profile.familyName,
+  //         profileImage: profile.picture,
+  //         email: profile.emails[0].value,
+  //       //   accessToken: accessToken,
+  //         // provider: profile.provider,
+  //         // passwordLink: req.body.passwordLink,
+  //       });
+  //       await adminGoogle.save();
+  //       // console.log(profile);
+  //     }
+
+  //     const { googleId, _id, createdAt, updatedAt, email, passwordLink } = adminGoogle;
+  // //           // Return a response to the client
+  //           res.status(200).json({
+  //             message: "Account created successfully",
+  //             status: "Success",
+  //             data: {
+  //              adminGoogle: {
+  // //                 firstName: firstName,
+  // //                 lastName: lastName,
+  //                 email: email,
+  //                 AdminId: _id,
+  //                 GoogleId: googleId,
+  //                 createdAt: createdAt,
+  //                 updatedAt: updatedAt,
+  //                 passwordLink: passwordLink,
+                // },
+              // },
+            // });
 
   //           if (error) {
   //             console.log(error);
