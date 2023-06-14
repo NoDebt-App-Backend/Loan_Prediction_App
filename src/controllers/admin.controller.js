996d20f5fc86a87f2e1d8380ac10dd75eaf60211
Perpy-del <perpetualmeninwa@gmail.com>
2023-06-14 08:27:00 +0100

    Added default image

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -145,6 +145,7 @@ export default class AdminController {
     if (existingCompany)
       throw new BadUserRequestError("Company name already exists");
 
+    const imageUrl = "https://nodebt-photosbucket.s3.us-east-1.amazonaws.com/User-Icon-Grey-300x300.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIA2PPOPHMTJ73UG25L%2F20230613%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230613T201841Z&X-Amz-Expires=3600&X-Amz-Signature=d72e1dd2227f011987a4a8f94ec969e0fd686102b0053c89dca433d23184b201&X-Amz-SignedHeaders=host&x-id=GetObject"
     // Create new admin account
     const admin = new Admin({
       firstName: req.body.firstName,
