29c0cdde0513cd41217cb80dd7db535ad4103ffb
Perpy-del <perpetualmeninwa@gmail.com>
2023-06-21 11:44:12 +0100

    New Dev for Google Authentication

--- src/controllers/googleAuth.controller.js
+++ src/controllers/googleAuth.controller.js
@@ -1,21 +1,29 @@
-// import axios from "axios"
+import axios from "axios";
 
-// const userInfo = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
-//           headers: { Authorization: `Bearer ${token}` } })
-//         .then((res) => res.data);
+export async function getGoogleToken(req, res) {
+  const token = req.body.token;
+  console.log(token);
+  // const adminCompanyMap = await AdminCompanyMap.find();
+  await axios
+    .get("https://www.googleapis.com/oauth2/v3/userinfo", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then((res) => res.data);
 
+  console.log(userInfo);
 
-//   console.log(userInfo);
+  res.status(200).json({
+    status: "Success",
+    message: "Token returned",
+    data: {
+      token,
+    },
+  });
+}
+// const userInfo = await axios
+//   .get("https://www.googleapis.com/oauth2/v3/userinfo", {
+//     headers: { Authorization: `Bearer ${token}` },
+//   })
+//   .then((res) => res.data);
 
-export const getGoogleToken = (req, res) => {
-    const token = req.body.token;
-    // const adminCompanyMap = await AdminCompanyMap.find();
-    res.status(200).json({
-      status: "Success",
-      message: "Token returned",
-      data: {
-        token
-      },
-    });
-    if (error) throw new InternalServerError("Internal Server Error");
-  }
+// console.log(userInfo);
