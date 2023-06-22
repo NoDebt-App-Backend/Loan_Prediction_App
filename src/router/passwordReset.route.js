c72db58e0da1aa5d7fd18dacd21f3f5f126a281f
Perpy-del <perpetualmeninwa@gmail.com>
2023-06-22 06:32:36 +0100

    Reset password

--- src/router/passwordReset.route.js
+++ src/router/passwordReset.route.js
@@ -11,6 +11,9 @@ router.get("/", tryCatchHandler(PasswordController.changePassword));
 /*Description: Passsword reset request*/
 router.post("/:id", tryCatchHandler(PasswordController.sendToken));
 
+/*Description: Passsword reset request*/
+router.post("/reset/:id", tryCatchHandler(PasswordController.resendToken));
+
 /*Description: Input New Password request*/
 router.put("/:id", tryCatchHandler(PasswordController.updateNewPassword));
 
