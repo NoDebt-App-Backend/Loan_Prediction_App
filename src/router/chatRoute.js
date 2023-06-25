66f4fc5bd154127aea3ca77aedaf8cd4e97f94e1
Perpy-del <perpetualmeninwa@gmail.com>
2023-06-25 19:56:09 +0100

    fixed bugs on chat controller

--- src/router/chatRoute.js
+++ src/router/chatRoute.js
@@ -2,7 +2,7 @@ import express from "express";
 import { chatAuth } from "../middlewares/chatAuth.js";
 const router = express.Router();
 
-import ChatController from "../controllers/chatController.js";
+import ChatController from "../controllers/ChatController.js";
 
 router.post("/", chatAuth, ChatController.accessChat);
 router.get("/", chatAuth, ChatController.fetchChats);
