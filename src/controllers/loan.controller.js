29b0d4fe287f1fe06099c4721ec377433b3448e1
Perpy-del <perpetualmeninwa@gmail.com>
2023-07-21 11:21:24 +0100

    Fixed issues with response from data science API

--- src/controllers/loan.controller.js
+++ src/controllers/loan.controller.js
@@ -42,31 +42,31 @@ export default class loanControllers {
     loan.organisationName = adminCompanyMap.organisationId.organisationName;
     const { createdAt } = loan;
 
-    // const response = await axios.post(
-    //   "https://fastapiproject-production.up.railway.app/loan_default_prediction",
-    //   {
-    //     gender: loan.gender,
-    //     marital_status: loan.maritalStatus,
-    //     employment: loan.employmentType,
-    //     income_per_month: loan.incomePerMonth,
-    //     loan_type: loan.loanType,
-    //     collateral_type: loan.collateralType,
-    //     collateral_value: loan.collateralValue,
-    //     guarantor_relationship: loan.guarantor.relationship,
-    //     guarantor_employment: loan.guarantor.employmentType,
-    //     guarantor_other_sources_of_income: loan.guarantor.otherSourcesOfIncome,
-    //     guarantor_income_per_month: loan.guarantor.incomePerMonth,
-    //     loan_amount: loan.loanAmount,
-    //     applicant_job_role: loan.jobRole,
-    //     applicant_job_sector: loan.jobSector,
-    //     age: loan.age,
-    //     guarantor_age: loan.guarantor.age,
-    //   }
-    // );
+    const response = await axios.post(
+      "https://web-production-f0d4.up.railway.app/loan_default_prediction",
+      {
+        gender: loan.gender,
+        marital_status: loan.maritalStatus,
+        employment: loan.employmentType,
+        income_per_month: loan.incomePerMonth,
+        loan_type: loan.loanType,
+        collateral_type: loan.collateralType,
+        collateral_value: loan.collateralValue,
+        guarantor_relationship: loan.guarantor.relationship,
+        guarantor_employment: loan.guarantor.employmentType,
+        guarantor_other_sources_of_income: loan.guarantor.otherSourcesOfIncome,
+        guarantor_income_per_month: loan.guarantor.incomePerMonth,
+        loan_amount: loan.loanAmount,
+        applicant_job_role: loan.jobRole,
+        applicant_job_sector: loan.jobSector,
+        age: loan.age,
+        guarantor_age: loan.guarantor.age,
+      }
+    );
 
-    loan.creditScore = 300;
+    loan.creditScore = response.data.creditScore;
     if (loan.incomePerMonth * 12 + loan.collateralValue > 2 * loan.loanAmount) {
-      loan.eligibility = true;
+      loan.eligibility = response.data.loanEligibility;
     } else {
       loan.eligibility = false;
     }
